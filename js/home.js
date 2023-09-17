ed8dead3f1dbb9908d42d14657c4dd288fcbad46
mattgia49 <mattgia49@gmail.com>
2023-09-16 21:11:40 -0400

    Random Text Changes

--- js/home.js
+++ js/home.js
@@ -1,3 +1,25 @@
-document.addEventListener("load", function(){
-    console.log(document.getElementById("homeText"))
+window.addEventListener("load", function(){
+    document.getElementById("homeText").innerText = randomString()
+    console.log(randomColor())
 })
+
+document.getElementById("homeText").addEventListener("mousemove", function(){
+    document.getElementById("homeText").innerText = randomString()
+    // document.getElementById("homeText").style.color = randomColor()
+})
+
+function randomString(){
+    let chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    let word = ""
+    for(let i = 0; i < 765; i++){
+        word += chars[Math.floor(Math.random() * chars.length)]
+    }
+    return word
+}
+
+function randomColor(){
+    let r = Math.floor(Math.random() * 255)
+    let g = Math.floor(Math.random() * 255)
+    let b = Math.floor(Math.random() * 255)
+    return "rgb(" + r + ", " + g + ", " + b + ")"
+}
