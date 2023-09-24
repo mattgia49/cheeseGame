3f6ff485c70e2f05e4707de6a5cd9cd89010d4f8
Ubuntu <ubuntu@ip-172-26-3-107.ec2.internal>
2023-09-24 22:59:06 +0000

    refactor

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/cheese")
+def cheese():
+    return render_template("cheese.html")
+
+app.run(host="0.0.0.0", port=80)
